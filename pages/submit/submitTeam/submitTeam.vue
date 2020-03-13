217a9ab39f1b841f12fcdaac2ecfe993104c88fc
txfs19260817 <zeminjiang@mail.ru>
2020-03-13 23:23:21 +0800

    first commit

---
+++ pages/submit/submitTeam/submitTeam.vue
@@ -0,0 +1,22 @@
+<template>
+	<view>
+		submit team
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+
+</style>
