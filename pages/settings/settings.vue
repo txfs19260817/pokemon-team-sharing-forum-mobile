43456cd6b6d3d1987d7f596f22c44533db5d8fff
txfs19260817 <zeminjiang@mail.ru>
2020-03-14 12:44:45 +0800

    finish about page

---
+++ pages/settings/settings.vue
@@ -0,0 +1,27 @@
+<template>
+	<view>
+		<uni-list>
+			<navigator url="about/about" hover-class="navigator-hover">
+				<uni-list-item title="关于" />
+			</navigator>
+			<!-- <uni-list-item title="语言/Language" rightText="zh-hans" :disabled="true" /> -->
+		</uni-list>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/settings/web-view/web-view.vue
@@ -0,0 +1,37 @@
+<template>
+	<view>
+		<web-view :webview-styles="webviewStyles" :src="url"></web-view>
+	</view>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				url: 'https://pokeshare.monster',
+				webviewStyles: {
+					progress: {
+						color: '#c83e3e'
+					}
+				}
+			}
+		},
+		onLoad(options) {
+			if (options && options.url) {
+				this.url = options.url;
+			}
+		},
+		methods: {
+			getMessage(event) {
+				uni.showModal({
+					content: JSON.stringify(event.detail),
+					showCancel: false
+				});
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
